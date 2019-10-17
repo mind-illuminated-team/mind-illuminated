334db1653f0825c59bcaa2069833dd4a26b1c356
Krisztian KERN <krisztian.kern@shiwaforce.com>
2019-10-17 10:41:29 +0200

    GoogleVR demo experimentation with Simple city plain and SplineMesh assets included

---
+++ GoogleVR/Assets/GoogleVR/Scripts/Cardboard/PatrolMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolMovement : MonoBehaviour
+{
+    public float startPoint;
+    public float endPoint;
+    public float gravityAcceleration;
+    float speed = 0;
+    bool forward = true;
+    Vector3 rotationVector = new Vector3(0, 180, 0);
+
+    float distance;
+    void Start() {
+        distance = endPoint - startPoint;
+    }
+
+    void Update () {
+        float y = transform.position.y;
+        if (forward) {
+            if (y <= endPoint) {
+                transform.Translate(Vector3.up * Time.deltaTime * 2);
+            } else {
+                //transform.Rotate(rotationVector);
+                forward = false;
+            }
+        } else {
+            if (y >= startPoint) {
+                speed += gravityAcceleration;
+                transform.Translate(Vector3.down * Time.deltaTime * Time.deltaTime * speed);
+            } else {
+                //transform.Rotate(rotationVector);
+                speed = 0;
+                forward = true;
+            }
+        }
+    }
+}
