ed7504e74360c991cec32af153f3cbe9c1f82d60
Krisztian Kern <krisz.kern@gmail.com>
2019-12-03 21:06:40 +0100

    SensorDataBroadcasterMock

--- udp-broadcast.py
+++ udp-broadcast.py
@@ -1,17 +1,52 @@
 import socket
 import time
+import random
 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-# Set a timeout so the socket does not block
-# indefinitely when trying to receive data.
-server.settimeout(0.2)
-server.bind(("", 44444))
+class SensorDataBroadcasterMock:
 
-a = 0
+    def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        # Set a timeout so the socket does not block
+        # indefinitely when trying to receive data.
+        self.server.settimeout(0.2)
+        self.server.bind(("", 44444))
 
-while True:
-    server.sendto(str.encode(str(a)), ('<broadcast>', 5000))
-    a = a + 1
-    print("message sent!")
-    time.sleep(1)
+        self.levels = (600, 500, 400, 300, 200, 100)
+        self.currentLevel = 0
+        self.deviationHalf = 20
+        self.levelDurationDeviationHalf = 3
+        self.baseLevelDuration = 8
+        self.levelDuration = self.baseLevelDuration
+        self.startTime = time.time()
+
+    def randomizeLevelDuration(self):
+        lowerBound = self.baseLevelDuration - self.levelDurationDeviationHalf
+        upperBound = self.baseLevelDuration + self.levelDurationDeviationHalf
+        self.levelDuration = random.randint(lowerBound, upperBound)
+
+    def switchLevel(self):
+        currentTime = time.time()
+        if (currentTime - self.startTime) > self.levelDuration:
+            self.currentLevel = random.randint(0, len(self.levels) - 1)
+            self.startTime = currentTime
+            self.randomizeLevelDuration()
+
+    def getRandomNumber(self):
+        levelValue = self.levels[self.currentLevel]
+        lowerBound = levelValue - self.deviationHalf
+        upperBound = levelValue + self.deviationHalf
+        return random.randint(lowerBound, upperBound)
+
+    def start(self):
+        print("Starting signal sending")
+        while True:
+            self.switchLevel()
+            number = self.getRandomNumber()
+            print(number)
+            self.server.sendto(str.encode(str(number)), ('<broadcast>', 5000))
+
+            time.sleep(0.05)
+
+if __name__ == '__main__':
+    SensorDataBroadcasterMock().start()
